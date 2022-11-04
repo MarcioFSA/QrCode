450d2b0bfe91dd649a98b78b767f197f68264bda
MarcioFSA <102062542+MarcioFSA@users.noreply.github.com>
2022-11-03 22:19:46 -0300

    Time

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-from datetime import date
+from datetime import date, datetime
 import qrcode
 
-data = date.today()
+data = datetime.today()
 img = qrcode.make(f'Setor de TI {data}')
 img.show()
 # print(img)
